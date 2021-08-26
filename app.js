35d5f12230cb6bc5b96653bf418ab059ad7eb2dd
abhishek-kamble <abhishek.kamble@walchandsangli.ac.in>
2021-08-27 00:22:00 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -3,15 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
     var today = new Date();
-    var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-    var day = daylist[today.getDay()];
-    res.render("list", {kindOfDay: day});
+    var options = {
+        weekend: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItem: items
+    });
 });
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server is running at port: 3000");
